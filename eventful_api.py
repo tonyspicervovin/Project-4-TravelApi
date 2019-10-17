1eb2a8a8d08fabd67e8d3910ec934ea8e5488503
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-17 17:24:04 -0500

    Fetch needed data for event information using eventful api

--- eventful_api.py
+++ eventful_api.py
@@ -1,11 +1,26 @@
 import requests
 
-def main():
-    # title, venue_name, venue_start_time, 
-    url = 'http://api.eventful.com/json/events/search?&app_key=bgw8NQ28vRcNxKHB'
-    params = {'keyword': 'musics', 'location': 'Minneapolis'}
-    data = requests.get(url, params=params).json()
-    print(data)
+def get_event_info():
+    # title, venue_name, venue_start_time,
 
-if __name__ == '__main__':
-    main()
+    location = user_input_location()
+    url = 'http://api.eventful.com/json/events/search?&app_key=bgw8NQ28vRcNxKHB'
+    params = {'keyword': 'musics', 'location': location}
+    data = requests.get(url, params=params).json()
+
+    event_data = data['events']['event']
+
+    for data in event_data:
+        name = data['title']
+        place = data['venue_name']
+        address = data['venue_address']
+        date = data['start_time']
+        print(f'Name: {name}')
+        print(f'Place: {place}')
+        print(f'Address: {address}')
+        print(f'Date: {date} \n')
+    
+def user_input_location():
+
+    location_input = input('Enter the location for more information: ')
+    return location_input
